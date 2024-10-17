1e34936ef14da15346e7a573662ea23f012774a8
bobsfriend12 <calebsethherring+github@gmail.com>
2024-10-16 22:12:46 -0500

    add buildscripts, fix indentation

--- src/main/java/team/gif/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/team/gif/robot/subsystems/ExampleSubsystem.java
@@ -4,11 +4,10 @@
 
 package team.gif.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem() {}
 
 }
